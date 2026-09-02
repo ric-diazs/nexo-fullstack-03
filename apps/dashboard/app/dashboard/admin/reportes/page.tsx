835c08ed4ba27aed7945d7db1ab0225f12147768
Remi Garcia Herrera <rem.garcia@duocuc.cl>
2026-09-02 19:46:32 -0400

    Add placeholder paragraph to reportes page

--- apps/dashboard/app/dashboard/admin/reportes/page.tsx
+++ apps/dashboard/app/dashboard/admin/reportes/page.tsx
@@ -3,6 +3,7 @@ export default function ReportesPAgePage() {
     <div>
       <h1 className="text-2xl font-bold text-gray-900 mb-2">Reportes</h1>
       <p className="text-gray-500">Módulo en construcción.</p>
+      <p  a benja le gusta la tula/>
     </div>
   )
 }
